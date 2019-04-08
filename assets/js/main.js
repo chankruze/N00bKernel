6b936fa06b066beb983dd0b628a32625840f83ae
Chandan Kumar Mandal <chankruze@gmail.com>
2019-04-08 05:37:57 +0530

    NK: Update Webiste Layout, Contents

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
-var quotes = [
-    "N00biness Overloaded !",
-    "You are a n00b !",
-    "This kernel will bomb your GF !",
-    "N00b devs here !",
-    "Funk Your Girl !"
+let quotes = [
+    "Tell me I’m your slut.",
+    "It may be between your legs, but it belongs to me. Understand?",
+    "Eyes on me, baby. Always on me!",
+    "I want you in my bed and between my legs.",
+    "I swear to God I won’t stop until you’re shaking.",
+    "Be naked when I get home.",
+    "This kernel will bomb your GF.",
+    "I want to taste me on your fingers.",
+    "I licked it, so it’s mine.",
+    "Shut up and kiss me.",
+    "Go ahead. Touch it."
 ];
 // Troll Myself
 let troll_index = Math.floor(Math.random() * quotes.length);
